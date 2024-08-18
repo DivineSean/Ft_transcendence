0a31f4cb45e01cc013f748bff2edcbfa7278fe18
driely01 <elyaagoubi.works@gmail.com>
2024-08-18 17:36:33 +0100

    add: Sign Up
    add blur to the background and change the title of the project

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 const SignUp = () => {
 	return (
 		<>
+		<div className="absolute min-h-screen w-screen backdrop-blur-xl"></div>
 		<div className="max-w-[1440px] m-auto lg:px-32 md:px-16 md:py-32 flex flex-col lg:gap-32 gap-16 min-h-screen">
 			<div className="lg:grid lg:grid-cols-[1fr_1fr] login-glass overflow-hidden flex flex-col grow md:rounded-[8px] md:border-[0.5px] md:border-stroke-pr">
 				<div className="bg-[url('/images/login/register.jpeg')] bg-cover bg-bottom flex flex-col">
@@ -13,7 +14,7 @@ const SignUp = () => {
 				<div className="md:px-64 px-32 flex flex-col justify-center md:gap-32 gap-24 lg:py-64 py-32 grow">
 					<div className="flex flex-col gap-8">
 						<h1 className="md:text-h-lg-xl text-h-sm-xl font-bold">get started</h1>
-						<p className="md:text-txt-lg text-txt-sm">Welcome back! Please enter your details</p>
+						<p className="md:text-txt-lg text-txt-sm">create your account now</p>
 					</div>
 					<form className="md:py-32 py-16 flex flex-col md:gap-32 gap-16">
 						<input type="text" required className="py-16 login-input border-b-2 border-stroke-sc" placeholder="Username"/>
@@ -22,7 +23,7 @@ const SignUp = () => {
 						<div className="flex justify-end">
 							<a href="#" className="underline">forget password?</a>
 						</div>
-						<button className="bg-green text-black font-bold py-8 rounded capitalize">register</button>
+						<button className="bg-green text-black text-h-sm-lg font-bold py-8 rounded capitalize">register</button>
 						<div className="flex gap-8 justify-center md:text-txt-md text-txt-sm">
 							<p className="">already have an account?</p>
 							<Link to='/login' className="font-bold">log in</Link>
