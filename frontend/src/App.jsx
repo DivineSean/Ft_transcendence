1f104f855cda17af792d79856c231cce39265ceb
driely01 <elyaagoubi.works@gmail.com>
2024-08-18 17:14:59 +0100

    add: Sign Up
    add sign up page and react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import Login from './components/Login'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import SignUp from './components/SignUp'
 
 function App() {
   return (
     <>
-			<Login />
+			<BrowserRouter>
+				<Routes>
+					<Route path='login/' element={<Login />} />
+					<Route path='register/' element={<SignUp />} />
+				</Routes>
+			</BrowserRouter>
     </>
   )
 }
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -2,26 +2,30 @@ import { Si42 } from "react-icons/si";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
 	return (
 		<>
 		<div className="max-w-[1440px] m-auto lg:px-32 md:px-16 md:py-32 flex flex-col lg:gap-32 gap-16 min-h-screen">
 			<div className="lg:grid lg:grid-cols-[1fr_1fr] login-glass overflow-hidden flex flex-col grow md:rounded-[8px] md:border-[0.5px] md:border-stroke-pr">
+				<div className="bg-[url('/images/login/register.jpeg')] bg-cover bg-bottom flex flex-col">
+					<div className="cover-gradient grow"></div>
+				</div>
 				<div className="md:px-64 px-32 flex flex-col justify-center md:gap-32 gap-24 lg:py-64 py-32 grow">
 					<div className="flex flex-col gap-8">
-						<h1 className="md:text-h-lg-xl text-h-sm-xl font-bold">Welcome back, Player</h1>
+						<h1 className="md:text-h-lg-xl text-h-sm-xl font-bold">get started</h1>
 						<p className="md:text-txt-lg text-txt-sm">Welcome back! Please enter your details</p>
 					</div>
 					<form className="md:py-32 py-16 flex flex-col md:gap-32 gap-16">
+						<input type="text" required className="py-16 login-input border-b-2 border-stroke-sc" placeholder="Username"/>
 						<input type="text" required className="py-16 login-input border-b-2 border-stroke-sc" placeholder="Email or username"/>
 						<input type="text" required className="py-16 login-input border-b-2 border-stroke-sc" placeholder="Password"/>
 						<div className="flex justify-end">
 							<a href="#" className="underline">forget password?</a>
 						</div>
-						<button className="bg-green text-black font-bold py-8 rounded">Log In</button>
+						<button className="bg-green text-black font-bold py-8 rounded capitalize">register</button>
 						<div className="flex gap-8 justify-center md:text-txt-md text-txt-sm">
-							<p className="">don't have an account?</p>
-							<Link to='/register' className="font-bold">Sign up</Link>
+							<p className="">already have an account?</p>
+							<Link to='/login' className="font-bold">log in</Link>
 						</div>
 					</form>
 					<div className="flex gap-16 items-center">
@@ -38,13 +42,10 @@ const Login = () => {
 						<p>log in with intra</p>
 					</a>
 				</div>
-				<div className="bg-[url('/images/login/login.jpeg')] bg-cover bg-bottom flex flex-col">
-					<div className="cover-gradient grow"></div>
-				</div>
 			</div>
 		</div>
 		</>
 	)
 }
 
-export default Login
+export default SignUp
