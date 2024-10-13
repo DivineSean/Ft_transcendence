c8fe2cfb111a46eb34f6ef7aa70d784028ae1ddf
Mohamed Arkik <markik@e1r4p13.1337.ma>
2024-10-13 14:57:05 +0100

    Fixing Profile Component

---
+++ frontend/src/components/ProfileFriends.jsx
@@ -0,0 +1,264 @@
+import { IoSearchSharp } from "react-icons/io5";
+import { TiThMenu } from "react-icons/ti";
+
+const ProfileFriends = () => {
+
+    return (
+    <div className='flex flex-col gap-16'>
+        <div className="flex w-full h-[32px] gap-10 place-content-end">
+            <div className="flex secondary-glass gap-16 p-16 items-center justify-center">
+                <IoSearchSharp />
+                <input
+                    type="text"
+                    placeholder="Find friends"
+                    className="flex text-txt-sm bg-transparent focus:outline-none"
+                />
+            </div>
+        </div>
+        <div className="flex gap-32">
+            {/* Ligne 1*/}
+            <div className="flex flex-col h-full w-[330px] gap-8">
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/* copier */}
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/*copier end*/}
+            </div>
+            {/* Ligne 2*/}
+            <div className="flex flex-col h-full w-[330px] gap-8">
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/* copier */}
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/*copier end*/}
+            </div>
+            {/* Ligne 3*/}
+            <div className="flex flex-col h-full w-[330px] gap-8">
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/* copier */}
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                <div className="flex secondary-glass h-[72px] w-full gap-16 p-8 items-center">
+                    <img 
+                        src="images/profile.png" 
+                        alt="Friend Personal image"
+                        className="flex rounded-full w-[56px] h-[56px]"
+                    />
+                    <div className="flex flex-col justify-center">
+                        <div className="text-txt-xs font-bold cursor-pointer">savannah nguyen</div>
+                        <div className="text-txt-xs">@snguyen</div>
+                    </div>
+                    <div className="flex items-center justify-center h-[24px] w-[55px] gap-10 px-12 py-4 border rounded-[4px] border-transparent bg-green text-black txt-xs font-medium cursor-pointer">invite</div>
+                    <div className="flex w-[24px] h-[24px] items-center justify-center cursor-pointer">
+                        <TiThMenu />
+                    </div>
+                </div>
+                {/*copier end*/}
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default ProfileFriends;
---
+++ frontend/src/components/ProfileOverview.jsx
@@ -0,0 +1,58 @@
+import { MdGames } from "react-icons/md";
+import { FaFire } from "react-icons/fa6";
+
+const ProfileOverview  = () => {
+    return (
+    <div className='flex gap-32'>
+        <div className='flex flex-col h-[288px] rounded-[8px] border-[0.5px] border-stroke-sc overflow-hidden'>
+            <div className='p-32 relative h-[150px] overflow-hidden flex items-end'>
+                <img 
+                    className='absolute top-0 left-0 w-full'
+                    src="images/pong-overview.webp" 
+                    alt=""
+                />
+                <div className='absolute top-0 left-0 w-full h-full overflow-hidden cover-gradient'></div>
+                <h2 className='absolute font-bold text-h-lg-lg'>ping pong</h2>
+            </div>
+            <div className='flex p-32 py-16 gap-32 h-[69px] items-center'>
+                <MdGames className="text-green text-txt-3xl" />
+                <div className='flex gap-40 justify-between grow'>
+                    <div className='flex flex-col'>
+                        <span className='text-h-lg-sm'>12</span>
+                        <span className='text-txt-xs'>total played</span>
+                    </div>
+                    <div className='flex flex-col'>
+                        <span className='text-h-lg-sm'>100%</span>
+                        <span className='text-txt-xs'>win rate</span>
+                    </div>
+                    <div className=''>
+                        <span className='flex uppercase text-green font-bold'>
+                            wwww
+                        </span>
+                        <span className='flex text-txt-xs'>recent result</span>
+                    </div>
+                </div>
+            </div>
+            <div className='flex sc-overview-glass p-32 py-16 gap-32 h-[69px] items-center'>
+                <FaFire className="text-red text-txt-3xl" />
+                <div className='flex gap-40 justify-between grow'>
+                    <div className='flex flex-col'>
+                        <span className='text-h-lg-sm'>0</span>
+                        <span className='text-txt-xs'>streak</span>
+                    </div>
+                    <div className='flex flex-col'>
+                        <span className='text-h-lg-sm'>22</span>
+                        <span className='text-txt-xs'>Rank</span>
+                    </div>
+                    <div className=''>
+                        <span className=''>36</span>
+                        <span className='flex text-txt-xs'>point</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default ProfileOverview;
