e1ffd781a48c69b00bcea6fd3d79129295a037b2
Saad Ouledlafqui <souledla@e1r3p7.1337.ma>
2024-10-24 18:34:06 +0100

    added chat, only using templates, figuring out the famous error, and trying to apply chat to users

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,22 +1,28 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 from asgiref.sync import async_to_sync
+from .models import ChatMessage
+from django.utils import timezone
+
+
+
+
 
 
 class Chat(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        # print("heeeeeeeeere     ",self.room_name, flush=True)
+        
         self.room_group_name = f'chat_{self.room_name}'
-        # print("Channel NAAAME : ",self.channel_name, flush=True)
-        # # Join room group
+        
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
 
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -26,7 +32,11 @@ class Chat(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        message_data = {
+            "message" : message,
+            "timestamp" : timezone.now()          
+        }
+        # await self.save_message(message_data)
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -43,4 +53,13 @@ class Chat(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message
         }))
-    
+
+    # @database_sync_to_async
+    # def save_message(self, message_data):
+        
+    #     ChatMessage.objects.create(
+    #         room_name = self.room_name,
+    #         message=message_data['message'],
+    #         sender=self.user,
+    #         reated_at = message_data['timestamp']
+    #     )
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import django
+import os
 
-# Create your models here.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+class ChatMessage(models.Model):
+    roomName = models.CharField(max_length=255)
+    message = models.TextField(blank=True)
+    sender = models.ForeignKey('Auth.Users', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    
