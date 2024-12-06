1f6c897e47f2a004f6945a687cf6c43162aea223
medDestiny <misskinemohammed977@gmail.com>
2024-12-06 18:33:14 +0100

    Add 3d effect to arrow buttons and select button
    Added 3d effect to the buttons and split them into individual components

--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -3,123 +3,77 @@ import Match from "../games/Matchmaking";
 import Header from "../components/Header"
 import { useState, useRef, useEffect } from "react"
 import { FaLongArrowAltRight } from "react-icons/fa";
+import "../styles/bmoButtons.css"
 
-const GameCard = ({ image, name, description }) => {
-
-    useEffect(() => console.log(name, description), [])
+const Buttons = () => {
     return (
-        <div
-            className="flex-shrink-0 p-16 w-full h-full rounded-lg flex flex-col md:flex-row gap-32
-            justify-between basis-auto bg-gradient-to-t from-black/[0.8]"
-            style={{
-                backgroundImage: `linear-gradient(to top, black, transparent), ${image}`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}
-        >
-					<div className="flex flex-col">
-							<span className="text-h-lg-3xl font-bold normal-case">{name}</span>
-							{/* <p className="min-w-0">{description}</p> */}
-							{/* <span className="text-txt-2xl normal-case text-gray line-clamp-2 hover:line-clamp-none overflow-hidden">{description}</span> */}
-					</div>
-					<div className="">
-							<button className="whitespace-nowrap gap-8 bg-green rounded-md p-16">
-									<span className="text-black">Play Now</span>
-									{/* <FaLongArrowAltRight className="text-black text-h-lg-lg" /> */}
-									{/* <svg className="size-32 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor"> */}
-									{/*     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" /> */}
-									{/* </svg> */}
-							</button>
-							{/* <button className="whitespace-nowrap gap-8 bg-green rounded-md"> */}
-							{/*     <h4>Play Now</h4> */}
-							{/*     <FaLongArrowAltRight className="text-black text-h-lg-lg" /> */}
-							{/* </button> */}
-					</div>
-        </div >
+        <div className="container-3d w-full h-[13.5vmin]">
+            <button id="up"></button>
+            <button id="left"></button>
+            <button id="right"></button>
+            <button id="down"></button>
+            <div className="axis is3d">
+                <div style={{ "--z": 1 }}></div>
+                <div style={{ "--z": 2 }}></div>
+                <div style={{ "--z": 3 }}></div>
+                <div style={{ "--z": 4 }}></div>
+                <div style={{ "--z": 5 }}></div>
+                <div style={{ "--z": 6 }}></div>
+            </div>
+        </div>
+    );
+};
+
+const Select = () => {
+    return (
+        <button id="select" className="container-3d is3d">
+            Select
+            <div style={{ "--z": 1 }}></div>
+            <div style={{ "--z": 2 }}></div>
+            <div style={{ "--z": 3 }}></div>
+            <div style={{ "--z": 4 }}></div>
+        </button>
     )
 }
 
 const Games = () => {
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const sliderRef = useRef(null)
-    const intervalRef = useRef(null)
     const games = [
         {
             name: "Pong",
-            description: "Description dyal had l game l wa3ra bzf, hh hh hh hh hhhhhh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh.",
+            description: "Description dyal had l game l wa3ra bzf, hh hhhhhhhhhhhhhhhhhh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hhhhhh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh.",
             image: "url('https://mir-s3-cdn-cf.behance.net/project_modules/fs/05daa256209423.59a540cb340e6.jpg')",
         },
-				{
-					name: "Pong",
-					description: "Description dyal had l game l wa3ra bzf, hh hh hh hh hhhhhh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh hh.",
-					image: "url('https://mir-s3-cdn-cf.behance.net/project_modules/fs/05daa256209423.59a540cb340e6.jpg')",
-				}
+        {
+            name: "Fleabag vs Mutt",
+            description: "Game dyal lklab hhhhh",
+            image: "url('https://www.gimori.com/images/cat-vs-dog.jpg')",
+        }
     ]
 
-    const updateSliderPosition = (index) => {
-        setCurrentIndex(index);
-        const gameCardWidth = sliderRef.current.offsetWidth;
-        // calculate offset using the card's width + gap (in this case 16px)
-        const offset = index * (16 + gameCardWidth)
-        sliderRef.current.style.transform = `translateX(-${offset}px)`;
-    };
-
-    const autoSlide = () => {
-        setCurrentIndex((prevIndex) => {
-            const nextIndex = (prevIndex + 1) % games.length;
-            updateSliderPosition(nextIndex);
-            return nextIndex;
-        })
-    }
-
-    // useEffect(() => {
-    //     intervalRef.current = setInterval(autoSlide, 5000);
-    //
-    //     return () => clearInterval(intervalRef.current);
-    // }, []);
 
     return (
         <div className="flex flex-col grow">
             <Header link="games" />
             <div className="backdrop-blur-sm w-full h-full absolute top-0 right-0"></div>
             <div className="container md:px-16 px-0">
-						{/* <div className="flex primary-glass p-16 lg:gap-32 gap-16 relative overflow-hidden get-height"> */}
-                <div className="primary-glass p-16 flex flex-col gap-16 grow">
-
-                    {/* Games Slider */}
-                    <div className="h-full rounded-lg grow overflow-hidden">
-                        <div
-                            className="h-full rounded-lg flex transition-transform gap-16 duration-1000 ease-in-out"
-                            ref={sliderRef}
-                        >
-                            {
-                                games.map((game, index) => (
-                                    <GameCard key={index} {...game} />
-                                ))
-                            }
+                <div className="primary-glass p-32 h-full">
+                    <div className="bg-[#2CA086] rounded-lg w-full h-full flex gap-16 p-16">
+                        <div className="grow max-w-[20%] flex flex-col justify-between items-center">
+                            <Select />
+                            <Buttons />
                         </div>
-                    </div>
-
-                    {/* Slider buttons */}
-                    <div className="p-8 flex gap-8 justify-center items-center">
-                        {
-                            games.map((_, index) => (
-
-                                <button
-                                    key={index}
-                                    onClick={() => {
-                                        if (index !== currentIndex) {
-                                            updateSliderPosition(index)
-                                            clearInterval(intervalRef.current)
-                                            intervalRef.current = setInterval(autoSlide, 5000)
-                                        }
-                                    }}
-                                    className={`rounded-full ${index === currentIndex ? "w-[30px] bg-green" : "w-12 bg-[#288A58]"}
-                                                h-12 transition-all duration-1000 ease-in-out`}
-                                >
-                                </button>
-                            ))
-                        }
+                        <div className="grow flex flex-col gap-16">
+                            <div className="rounded-2xl overflow-hidden grow flex flex-col">
+                                <span className="bg-[#165044] h-[5%] w-full">header</span>
+                                <span className="bg-[#B2F5CE] w-full grow">screen</span>
+                            </div>
+                            <div className="h-[47px] flex justify-between">
+                                <span className="bg-[#165044] rounded-lg h-full w-[60%]"></span>
+                                <button className="bg-[#006680] rounded-full w-[47px] h-[47px]"></button>
+                            </div>
+                        </div>
+                        <div className="bg-red grow max-w-[20%]">
+                        </div>
                     </div>
                 </div>
             </div>
