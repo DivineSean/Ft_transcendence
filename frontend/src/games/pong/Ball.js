dc6e8795437796b9f7d829cc53af52bc1ecf77e3
Mohamed Arkik <markik@e1r9p2.1337.ma>
2024-12-17 14:46:22 +0100

    this is the latest PingPong Version of Mltiplayer Game work with everything, the only need is victory/loss effect and achievement to do later

--- frontend/src/games/pong/Ball.js
+++ frontend/src/games/pong/Ball.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
+import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 
 const G = 0.0009;
-const MAX_POINTS = 180;
 
 class Ball {
   constructor(scene, loader, player) {
@@ -9,6 +9,19 @@ class Ball {
     this.model = undefined;
     this.loader = loader;
     this.boundingSphere = undefined;
+    this.bounceSound = undefined;
+    this.netHitSound = undefined;
+    this.paddleHitSound = undefined;
+    this.onlyHit = undefined;
+    this.swing = undefined;
+    this.scoreSound = undefined;
+    this.lostSound = undefined;
+    this.BackgroundMusic = undefined;
+    this.ballMatchPoint = undefined;
+    this.GameOver = undefined;
+    this.Defeat = undefined;
+    this.Victory = undefined;
+    this.DorV = 0;
     this.player = player;
     this.radius = 0;
 
@@ -25,11 +38,39 @@ class Ball {
     this.serving = true;
     this.lastshooter = 1;
     this.sendLock = false;
+    this.startTime = Date.now();
+    // Set up CSS2DRenderer
+    this.labelRenderer = new CSS2DRenderer();
+    this.labelRenderer.setSize(window.innerWidth, window.innerHeight);
+    this.labelRenderer.domElement.style.position = "absolute";
+    this.labelRenderer.domElement.style.top = "0px";
+    this.labelRenderer.domElement.style.pointerEvents = "none"; // Prevent interaction issues
+    document.body.appendChild(this.labelRenderer.domElement);
+    this.div = document.createElement("div");
+    this.div.className = "label";
+    this.div.textContent = "10 Seconds To Serve the Ball\n\t0s\t";
+    this.label = new CSS2DObject(this.div);
+    this.label.position.set(0, 0, 0);
+    this.scene.add(this.label);
+    this.timeout = false;
+    
   }
 
   serve(ws, net, sign) {
+    this.scene.remove(this.label);
+    if(this.player === 1 && sign === 1 || this.player === 2 && sign === -1)
+    {
+      this.scoreSound.currentTime = 0;
+      this.scoreSound.play();
+      this.scene.add(this.label);
+      this.div.textContent = "10 Seconds To Serve the Ball\n\t0s\t";
+    }
+    else
+    {
+      this.lostSound.currentTime = 0;
+      this.lostSound.play();
+    }
     this.x = 42 * sign;
-    // this.y = 6;
     this.y = net.boundingBox.max.y;
     this.z = -20 * sign;
 
@@ -38,9 +79,23 @@ class Ball {
     this.dz = 0;
     this.count = 0;
     this.serving = true;
+    this.timeout = false;
     this.lastshooter = sign;
     this.sendLock = false;
+    this.startTime = Date.now();
   }
+
+  CheckTimer(ws) {
+    const Timer = Math.floor((Date.now() - this.startTime) / 1000);
+    this.div.textContent = `10 Seconds To Serve the Ball ${Timer}s`;
+    
+    if (Timer === 10) {
+      this.sendLost(ws);
+      this.sendLock = true;
+      this.timeout = true;
+    }
+  }
+  
 
   sendScore(ws) {
     const data = { type: "score", message: {} };
@@ -48,7 +103,7 @@ class Ball {
   }
 
   sendLost(ws) {
-    // this.count = 0;
+    this.count = 0;
     this.sendLock = true;
     this.serving = true;
     const data = {
@@ -65,15 +120,6 @@ class Ball {
   }
 
   update(net, table, player1, ws, dt, player, keyboard) {
-    if (
-      !this.model ||
-      !net.boundingBox ||
-      !table.boundingBoxTable ||
-      !this.boundingSphere ||
-      !player1.boundingBox
-    )
-      return;
-    console.log(`count ${this.count}`);
     if (this.sendLock && this.serving && this.count >= 2) return;
     this.dy -= G;
     let flag = false;
@@ -81,19 +127,18 @@ class Ball {
     if (this.serving) this.count = 0;
     if (this.y < -50 && this.sendLock === false && this.serving === false) {
       if (this.lastshooter === 1 && player === 1) {
-        console.log("me 1");
         this.sendLost(ws);
       } else if (this.lastshooter === -1 && player === 2) {
-        console.log("me 2");
         this.sendLost(ws);
       }
       this.sendLock = true;
       this.serving = true;
-      // this.count = 0;
       return;
     } else if (this.boundingSphere.intersectsBox(table.boundingBoxTable)) {
       this.y = table.boundingBoxTable.max.y + 1;
       this.dy *= -0.6;
+      this.bounceSound.currentTime = 0;
+      this.bounceSound.play();
       if (this.sendLock === false && this.serving === false && this.count < 2) {
         this.count++;
         if (this.x < 0) this.lastshooter = -1;
@@ -101,27 +146,33 @@ class Ball {
         if (this.count === 2) {
           if (this.lastshooter === -1 && player === 2) {
             this.sendLost(ws);
-            console.log(`me 3`);
           } else if (this.lastshooter === 1 && player === 1) {
             this.sendLost(ws);
-            console.log("me 4");
           }
           return;
         }
       }
     }
+    let status = "";
     if (this.boundingSphere.intersectsBox(net.boundingBox)) {
+      this.netHitSound.currentTime = 0.5;
+      this.netHitSound.play();
       player1.netshoot(this, net, ws, player);
       this.count = 0;
       flag = true;
     }
-    // Paddles
+
+    //Paddles
     if (
       this.boundingSphere.intersectsBox(player1.boundingBox) &&
       player1.rotating &&
       this.sendLock === false
     ) {
+      this.paddleHitSound.currentTime = 0;
+      this.paddleHitSound.play();
       player1.shoot(net, keyboard, this, dt);
+      status = "shoot";
+      this.scene.remove(this.label);
       this.serving = false;
       this.lastshooter = player1.player;
       this.count = 0;
@@ -131,11 +182,20 @@ class Ball {
       !player1.rotating &&
       !this.serving
     ) {
+      this.onlyHit.currentTime = 0;
+      this.onlyHit.play();
+      status = "hit";
       player1.hit(this, ws);
+      this.scene.remove(this.label);
       this.serving = false;
       this.count = 0;
       this.lastshooter = player1.player;
       flag = true;
+    }
+    else if (player1.rotating)
+    {
+      this.swing.currentTime = 0;
+      this.swing.play();
     }
 
     // Movement
@@ -158,6 +218,7 @@ class Ball {
             dt: dt,
             serving: this.serving,
             lstshoot: this.lastshooter,
+            stats: status,
           },
         },
       };
@@ -191,9 +252,78 @@ class Ball {
       }
     });
 
+    const audioLoader = new THREE.AudioLoader();
+    const listener = new THREE.AudioListener();
+
+    // Load all sounds
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/bounce.mp3`, (buffer) => {
+      this.bounceSound = new THREE.Audio(listener);
+      this.bounceSound.setBuffer(buffer);
+      this.bounceSound.setVolume(0.8);
+    });
+    
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/NetHit.mp3`, (buffer) => {
+      this.netHitSound = new THREE.Audio(listener);
+      this.netHitSound.setBuffer(buffer);
+      this.netHitSound.setVolume(0.8);
+    });
+    
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/hit.mp3`, (buffer) => {
+      this.paddleHitSound = new THREE.Audio(listener);
+      this.paddleHitSound.setBuffer(buffer);
+      this.paddleHitSound.setVolume(0.8);
+    });
+    
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/BallHit.mp3`, (buffer) => {
+      this.onlyHit = new THREE.Audio(listener);
+      this.onlyHit.setBuffer(buffer);
+      this.onlyHit.setVolume(0.8);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/swing.mp3`, (buffer) => {
+      this.swing = new THREE.Audio(listener);
+      this.swing.setBuffer(buffer);
+      this.swing.setVolume(0.5);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/Music4.mp3`, (buffer) => {
+      this.BackgroundMusic = new THREE.Audio(listener);
+      this.BackgroundMusic.setBuffer(buffer);
+      this.BackgroundMusic.setVolume(0.1);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/scoring.mp3`, (buffer) => {
+      this.scoreSound = new THREE.Audio(listener);
+      this.scoreSound.setBuffer(buffer);
+      this.scoreSound.setVolume(0.5);
+    });
+
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/aww.mp3`, (buffer) => {
+      this.lostSound = new THREE.Audio(listener);
+      this.lostSound.setBuffer(buffer);
+      this.lostSound.setVolume(0.5);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/MatchPoint.mp3`, (buffer) => {
+      this.ballMatchPoint = new THREE.Audio(listener);
+      this.ballMatchPoint.setBuffer(buffer);
+      this.ballMatchPoint.setVolume(0.5);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/Defeat.mp3`, (buffer) => {
+      this.Defeat = new THREE.Audio(listener);
+      this.Defeat.setBuffer(buffer);
+      this.Defeat.setVolume(0.5);
+    });
+
+    audioLoader.load(`https://${window.location.hostname}:3000/src/games/pong/Sounds/Victory.mp3`, (buffer) => {
+      this.Victory = new THREE.Audio(listener);
+      this.Victory.setBuffer(buffer);
+      this.Victory.setVolume(0.5);
+    });
     this.scene.add(this.model);
 
-    // Optional: Dynamic bounding sphere calculation
     const box = new THREE.Box3().setFromObject(this.model);
     const center = box.getCenter(new THREE.Vector3());
     this.radius = box.getSize(new THREE.Vector3()).length() / 2;
