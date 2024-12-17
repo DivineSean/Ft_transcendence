06a31710e93335fe6dcad061f3fd67046c1b20a7
дестини <112112096+medDestiny@users.noreply.github.com>
2024-12-17 15:01:28 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,13 +107,10 @@ SIMPLE_JWT = {
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost:3000",
     "https://localhost:8000",
-    "https://10.11.9.2:3000",
-    "https://10.11.9.2:8000",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
-    "https://10.11.9.2:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
