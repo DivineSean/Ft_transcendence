aad829ccaddf1a6ea2a979117c0a6efeb8b40550
Saad Ouledlafqui <souledla@e1r3p13.1337.ma>
2024-12-23 21:10:30 +0100

    added some views

--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -3,25 +3,33 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Lobby, Player
 from uuid import UUID
-
+from .models import Match
+import math
 
 
 @api_view(["POST"])
 def CreateTournament(request):
     maxPlayers = request.data.get("maxPlayers")
     if not maxPlayers or not maxPlayers.isdigit():
-        return Response("Either maxPlayers not specified or notdigit", status = 400)
-    if int(maxPlayers) != 4 and int(maxPlayers) != 8 and int(maxPlayers) != 16:
-        return Response("maxPlayers should be 4/8/16", status = 400)
-    existingLobby = Lobby.objects.filter(creator = request._user) 
+        return Response({"error": "maxPlayers not specified or not a digit"}, status=400)
+    
+    maxPlayers = int(maxPlayers)
+    if maxPlayers not in [4, 8, 16]:
+        return Response({"error": "maxPlayers khas ykon 4, 8, wla 16"}, status=400)
+    
+    existingLobby = Lobby.objects.filter(creator=request._user, is_completed=False).first()
+
     if existingLobby:
-        return Response({"message":"User Already have active tournament", "roomID" : existingLobby.values('roomID').first()['roomID']})
-    newLobby = Lobby.objects.create( 
-        creator = request._user,
-        maxPlayers = int(maxPlayers)
+        return Response({"message": "User already has active tournament","lobbyID": str(existingLobby.lobbyID)})
+    
+    newLobby = Lobby.objects.create(
+        creator=request._user,
+        maxPlayers=maxPlayers
     )
     newLobby.addPlayer(request._user)
-    return Response({"message" : "Lobby Created Successfully","roomID" : str(newLobby.roomID)}, status= 201)
+    
+    return Response({"message": "Lobby created successfully","lobbyID": str(newLobby.lobbyID)}, status=201)
+
 
 @api_view(["POST"])
 def addPlayerToLobby(request):
@@ -40,8 +48,9 @@ def addPlayerToLobby(request):
     playerObj = lobby.addPlayer(request._user)
 
     if lobby.currentPlayerCount == lobby.maxPlayers:
-        # khsni n9ad brackets system
-        pass
+        createBracket(lobby)
+        #send matches to markik
+
     return Response({"message" : f'{playerObj[0]}'}, status = int(playerObj[1]))
 
 @api_view(["GET"])
@@ -52,4 +61,66 @@ def deleteTournament(request):
         return Response("No Lobby created by this Uuser", status = 400)
     lobby.delete()
     return Response({"Lobby Deleted"}, status = 200) 
+
+def createBracket(lobby):
+    players = list(lobby.players.all()) #example la kano 4 players = [p1,p2,p3,p4]
+    numRounds = int(math.log2(len(players))) #Reminder al kosala, logarithm howa l power d n , so ila user choosa tournament d 4 , then 2 rounds exists, 8 => 3 rounds exists, 16 => 4 rounds exists
     
+    for num in range(numRounds): # numRounds = 2
+        createMatches(lobby, players, num + 1) # in case numRounds = 2 donc num = 1 first round, num = 2 second round
+
+
+
+
+def createMatches(lobby, players, roundNum):
+    # matches = []
+    for i in range(0, len(players), 2): # 2 b 2  (i = 0 then 2 in case len = 4)
+        p1 = players[i] # 0 then  2 
+
+        if i + 1 < len(players): # i = 1 then 3 
+            p2 = players[i + 1] # 1 then 3
+        else : 
+            p2 = None
+
+        #match = we dont need match for now
+        Match.objects.create(
+            lobby=lobby, # same lobby logic
+            player1=p1, # 0, 2 (ids)
+            player2=p2, # 1, 3 (kandwi 3la l ids)
+            tnRound=roundNum # 1 the 2
+        )
+        
+        # matches.append(match) # no need daba
+
+@api_view(["POST"])
+def MarkikDirChi9lwa(request):
+    matchID = request.data.get("matchID")
+    winnerID = request.data.get("winnerID")
+
+    try:
+        match = Match.objects.get(id=matchID)
+        winner = Player.objects.get(id=winnerID)
+
+        
+        match.winner = winner
+        match.save()
+
+        
+        if match.player1 != winner:
+            loser = match.player1 
+        else:  
+            loser = match.player2
+        
+        # Player.objects.get(tounament = lobby, user != winner).delete() should fix this
+        
+        loser.delete() # deleti l 9lawi direct makayn la fix la walo , 3sbni
+        
+        if not Match.objects.filter(lobby=match.lobby, winner=None).exists():
+            winners = Player.objects.filter(tournament=match.lobby).exclude(matches__winner=None)
+            if len(winners) % 2 == 0:  # Ensure even number of winners for next round
+                createMatches(match.lobby, winners, match.tnRound + 1)
+
+        return Response({"message": "Match result updated successfully"}, status=200)
+
+    except :
+        return Response({"message": "Either matchID or winnerID not valid"}, status=404)
