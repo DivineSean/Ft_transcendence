30e15a45024146dfa6bcac3d66795af1f7d78300
Saad Ouledlafqui <souledla@e1r3p15.1337.ma>
2024-12-27 19:19:54 +0100

    removed junk file

--- backend/tournament/lol.py
+++
@@ -1,8 +0,0 @@
-channel_layer = get_channel_layer()
-                        group_name = f"notifications_{userId}"
-                        async_to_sync(channel_layer.group_send)(
-                            group_name,
-                            {
-                                "type": "send_friend_request",
-                                "sender": str(request._user.id),
-                            },
