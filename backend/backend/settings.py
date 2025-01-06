c6c6dc6a0498d227fa729742a25cf3434e1aa216
DivineSean <109292391+DivineSean@users.noreply.github.com>
2025-01-06 19:56:41 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,13 +101,10 @@ SIMPLE_JWT = {
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost:3000",
     "https://localhost:8000",
-    "https://10.11.3.15:3000",
-    "https://10.11.3.15:8000",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
-    "https://10.11.3.15:3000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
