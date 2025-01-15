0f31b945927ad9a9ed5052ba97ac82021cc4bb67
driely01 <elyaagoubi.works@gmail.com>
2025-01-15 23:18:15 +0100

    getTournamentData endpoint perfect

--- backend/tournament/serializers.py
+++ backend/tournament/serializers.py
@@ -5,35 +5,35 @@ from games.serializers import GameSerializer, PlayerSerializer
 from .serializers import PlayerSerializer
 
 class TournamentSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(format="hex_verbose", read_only=True)
+		id = serializers.UUIDField(format="hex_verbose", read_only=True)
 
-    class Meta:
-        model = Tournament
-        fields = ["id"]
+		class Meta:
+				model = Tournament
+				fields = ["id"]
 
 
 class getTournamentSerializer(serializers.ModelSerializer):
-    isCreator = serializers.SerializerMethodField()
-    gameData = GameSerializer(source="game", read_only=True)
-    created_at = serializers.DateTimeField(format="%b %d, %Y")
+		isCreator = serializers.SerializerMethodField()
+		gameData = GameSerializer(source="game", read_only=True)
+		created_at = serializers.DateTimeField(format="%b %d, %Y")
 
-    class Meta:
-        model = Tournament
-        fields = [
-            "id",
-            "isCreator",
-            "tournamentTitle",
-            "created_at",
-            "maxPlayers",
-            "currentPlayerCount",
-            "isCanceled",
-            "gameData",
-        ]
+		class Meta:
+				model = Tournament
+				fields = [
+						"id",
+						"isCreator",
+						"tournamentTitle",
+						"created_at",
+						"maxPlayers",
+						"currentPlayerCount",
+						"isCanceled",
+						"gameData",
+				]
 
-    def get_isCreator(self, obj):
-        if self.context == obj.creator:
-            return True
-        return False
+		def get_isCreator(self, obj):
+				if self.context == obj.creator:
+						return True
+				return False
 
 
 
@@ -42,40 +42,42 @@ class getTournamentSerializer(serializers.ModelSerializer):
 
 class TournamentPlayerSerializer(PlayerSerializer):  
 
-    # diro raskom bhala machftoch kan inheriti mn PlayerSerializer d mskin
-   
-    username = serializers.CharField(source='user.username')
-    profile_image = serializers.CharField(source='user.profile_image')
-    id = serializers.CharField(source='user.id')
-
-    class Meta:
-        model = Player
-        fields = [
-            "id",
-            "result",
-            "username", 
-            "profile_image",  
-        ]
+		
+	
+		username = serializers.CharField(source='user.username')
+		profile_image = serializers.CharField(source='user.profile_image')
+		id = serializers.CharField(source='user.id')
+		
+		class Meta:
+				model = Player
+				fields = [
+						"id",
+						"result",
+						"score",
+						"username", 
+						"profile_image",  
+				]
 
 class TournamentDataSerializer(serializers.Serializer):
-    def to_representation(self, instance):
-        data = {}
-        brackets = instance.get("brackets")  
+		def to_representation(self, instance):
+				data = []
+				brackets = instance.get("brackets")  
 
-        
-        for bracket in brackets:
-            gameRooms = GameRoom.objects.filter(bracket=bracket)
-            bracketData = []
-            for gameRoom in gameRooms:
-                gameRoomiD = str(gameRoom.id)  
-                
-                playersData = TournamentPlayerSerializer(
-                    Player.objects.filter(game_room=gameRoom), many=True
-                ).data
+				
+				for bracket in brackets:
+						gameRooms = GameRoom.objects.filter(bracket=bracket)
+						bracketData = {}
+						for gameRoom in gameRooms:
+								gameRoomiD = str(gameRoom.id)  
+								
+								playersData = TournamentPlayerSerializer(
+										Player.objects.filter(game_room=gameRoom), many=True
+								).data
 
-                bracketData.append({
-                    gameRoomiD: playersData
-                })
-            data[str(bracket.id)] = bracketData 
+								bracketData[gameRoomiD] = playersData 
+								
+						# data[str(bracket.id)] = bracketData 
+						data.append(bracketData)  
+						print(data, flush=True)
 
-        return data
+				return {'region': data}
