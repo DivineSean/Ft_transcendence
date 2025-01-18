6d94bf13cb269b1d6678ed87d5fd537831db0ad0
driely01 <elyaagoubi.works@gmail.com>
2025-01-18 20:06:21 +0100

    add tabs

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -41,12 +41,12 @@ class Notifications(models.Model):
 				self.timestamp = datetime.now()
 				super().save(*args, **kwargs)
 
-    def get_default_message(self):
-        return {
-            "FR": "You have received a friend request from: ",
-            "IG": "You've been invited to a game by: ",
-            "IT": "You've been invited to a tournament by: ",
-            "ME": "You have received a message from: ",
-            "CC": "Conversation created by: ",
-            "AF": "Your friend request has been accepted by: ",
-        }.get(self.notifType, "finahowa l message d notif al 3yan")
+		def get_default_message(self):
+				return {
+						"FR": "You have received a friend request from: ",
+						"IG": "You've been invited to a game by: ",
+						"IT": "You've been invited to a tournament by: ",
+						"ME": "You have received a message from: ",
+						"CC": "Conversation created by: ",
+						"AF": "Your friend request has been accepted by: ",
+				}.get(self.notifType, "finahowa l message d notif al 3yan")
