b9b271b7dc4425b8ba7bfdf4406c1f684c2ec1e4
driely01 <elyaagoubi.works@gmail.com>
2025-01-18 17:08:04 +0100

    small fix

--- frontend/src/pages/Tournaments.jsx
+++ frontend/src/pages/Tournaments.jsx
@@ -12,7 +12,7 @@ const Tournaments = () => {
   const { uid } = useParams();
   const [displayCreateTournament, setDisplayCreateTournament] = useState(false);
   const [tournaments, setTournaments] = useState(null);
-  const [displayError, setDisplayError] = useState(false);
+  // const [displayError, setDisplayError] = useState(false);
 
   useEffect(() => {
     return () => {
@@ -54,16 +54,16 @@ const Tournaments = () => {
                 Create New Tournament
               </button>
             )}
-            {displayError && (
+            {/* {displayError && (
               <div className="absolute left-1/2 -translate-x-1/2 text-xs w-full px-16 bottom-4 text-center text-white md:text-red md:text-txt-sm">
                 the first round does not complete yet that's why you cannot see
                 the bracket right now. this is just the example.
               </div>
-            )}
+            )} */}
           </div>
           <div className="tournaments-height">
             {uid ? (
-              <Brackets setDisplayError={setDisplayError} />
+              <Brackets />
             ) : (
               <ListTournaments
                 tournaments={tournaments}
